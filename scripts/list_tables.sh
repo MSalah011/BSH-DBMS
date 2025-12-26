b07c73fe890c377eceed0436597bf92b3d4819fa
Mahmoud Salah <mahmoudalmasry0005@gmail.com>
2025-12-26 23:51:37 +0200

    feat:List all available tables in a database

---
+++ scripts/list_tables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# This script lists all tables in a specified database
+db_path="./Databases"
+database_name="$1"
+# check if database has tables
+if [ -z "$(ls -A "$db_path/$database_name")" ]; then
+    echo "No tables found in database '$database_name'."
+else
+    ls "$db_path/$database_name"
