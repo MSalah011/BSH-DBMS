70ebd51694cbb223492023fd6eeda47d4df91d7c
Mahmoud Salah <mahmoudalmasry0005@gmail.com>
2025-12-26 15:53:02 +0200

    feat: Create database menu

---
+++ scripts/db_menu.sh
@@ -0,0 +1,41 @@
+#! /bin/bash
+
+select option in "Create Table" "List Tables" "Drop Table" "Insert into Table" "Select From Table"  "Delete From Table" "Update Table" "Back to Main Menu"
+do 
+    case $REPLY in 
+        1) 
+        echo Create Table 
+        #./scripts/create_table.sh 
+        ;;
+        2) 
+        echo List tables 
+        #./scripts/list_tables.sh 
+        ;;
+        3) 
+        echo Drop Table 
+        #./scripts/drop_table.sh 
+        ;;
+        4) 
+        echo Insert into Table 
+        #./scripts/insert_into_table.sh 
+        ;;
+        5) 
+        echo Select from Table 
+        #./scripts/select_from_table.sh 
+        ;;
+        6) 
+        echo Delete from Table 
+        #./scripts/delete_from_table.sh 
+        ;;
+        7) 
+        echo Delete Table 
+        #./scripts/update_table.sh 
+        ;;
+        8) 
+        echo back to menu 
+        #./main.sh 
+        ;;
+        *) echo "Invalid option. Please try again." 
+        ;;
+    esac
+done
