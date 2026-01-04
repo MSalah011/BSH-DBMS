083e65092e29cde5282cec065d18068bc7182d4c
Mahmoud Salah <mahmoudalmasry0005@gmail.com>
2026-01-04 18:48:06 +0200

    fix:check for empty input in drop_DB

--- scripts/drop_databases.sh
+++ scripts/drop_databases.sh
@@ -4,6 +4,12 @@ db_path="./Databases"
 echo -n "Enter database name to drop: "
 read db_name
 
+#check for empty database name
+if [[ -z "$db_name" ]]; then
+    echo "Cannot drop database: name cannot be empty!"
+    exit 1
+fi
+
 # check database is exist
 if [[ -d "$db_path/$db_name" ]]; then
     echo -n "Are you sure you want to delete '$db_name'? (y/n): "
